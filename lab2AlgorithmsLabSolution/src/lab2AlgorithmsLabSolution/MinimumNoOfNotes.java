f2d94c1d02d7b82cc5b381c1c15faef1afedf7f3
ajeet <ajeetkumar23898@gmail.com>
2022-04-10 11:54:44 +0530

    commit

--- lab2AlgorithmsLabSolution/src/lab2AlgorithmsLabSolution/MinimumNoOfNotes.java
+++ lab2AlgorithmsLabSolution/src/lab2AlgorithmsLabSolution/MinimumNoOfNotes.java
@@ -1,5 +1,5 @@
 package lab2AlgorithmsLabSolution;
-
+ 
 import java.util.Arrays;
 
 import java.util.Scanner;
--- lab2AlgorithmsLabSolution/src/lab2AlgorithmsLabSolution/Paymoney.java
+++ lab2AlgorithmsLabSolution/src/lab2AlgorithmsLabSolution/Paymoney.java
@@ -1,5 +1,5 @@
 package lab2AlgorithmsLabSolution;
-
+ 
 import java.util.*;
 
 public class Paymoney {
